669eb2376ce21eb8618ea1f1ce25fa1354d100a8
kim-seonwoo <sw204011@gmail.com>
2024-04-03 19:26:58 +0900

    [Feat/#1] DatePicker 추가

--- 34th-Sopt-Seminar/34th-Sopt-Seminar/Source/VC/LoginViewController.swift
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/Source/VC/LoginViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 final class LoginViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        setLayout()
+    }
+    
+    private func setLayout() {
+        [titleLabel, idTextField, passwordTextField, loginButton, datePickerTextField].forEach {
+            self.view.addSubview($0)
+        }
+    }
+    
     private let titleLabel: UILabel = {
         let label = UILabel(frame: CGRect(x: 69, y: 161, width: 236, height: 44))
         label.text = "동네라서 가능한 모든것\n당근에서 가까운 이웃과 함께해요."
@@ -22,23 +35,36 @@ final class LoginViewController: UIViewController {
     
     private let idTextField: UITextField = {
         let textField = UITextField(frame: CGRect(x: 20, y: 276, width: 335, height: 52))
-        textField.placeholder = "   아이디"
+        textField.placeholder = "아이디"
         textField.font = UIFont(name: "Pretendard-SemiBold", size: 14)
-        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        textField.backgroundColor = .gray200
+        textField.layer.cornerRadius = 3
+        textField.tintColor = .gray
+        
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+         textField.leftView = paddingView
+         textField.leftViewMode = .always
         return textField
     }()
     
     private let passwordTextField: UITextField = {
         let textField = UITextField(frame: CGRect(x: 20, y: 335, width: 335, height: 52))
-        textField.placeholder = "   비밀번호"
+        textField.placeholder = "비밀번호"
+        textField.layer.cornerRadius = 3
         textField.font = UIFont(name: "Pretendard-SemiBold", size: 14)
-        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        textField.tintColor = .gray
+        textField.backgroundColor = .gray200
+        
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+         textField.leftView = paddingView
+         textField.leftViewMode = .always
+        
         return textField
     }()
     
     private lazy var loginButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 21, y: 422, width: 332, height: 58))
-        button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+        let button = UIButton(frame: CGRect(x: 21, y: 482, width: 332, height: 58))
+        button.backgroundColor = .primaryOrange
         button.layer.cornerRadius = 6
         button.setTitle("로그인하기", for: .normal)
         button.setTitleColor(.white, for: .normal)
@@ -47,17 +73,30 @@ final class LoginViewController: UIViewController {
         return button
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private let datePickerTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 395, width: 335, height: 52))
+        textField.placeholder = "날짜 선택"
+        textField.font = UIFont(name: "Pretendard-SemiBold", size: 14)
+        textField.backgroundColor = .gray200
+        textField.layer.cornerRadius = 3
+        textField.tintColor = .clear
+
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+         textField.leftView = paddingView
+         textField.leftViewMode = .always
         
-        self.view.backgroundColor = .white
-        setLayout()
-    }
-    
-    private func setLayout() {
-        [titleLabel, idTextField, passwordTextField, loginButton].forEach {
-            self.view.addSubview($0)
-        }
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.addTarget(self, action: #selector(datePickerValueChanged), for: .valueChanged)
+        textField.inputView = datePicker
+        return textField
+    }()
+
+    @objc private func datePickerValueChanged(sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM월 dd일 좋은 아침 입니다"
+        datePickerTextField.text = dateFormatter.string(from: sender.date)
     }
     
     private func presentToWelcomeVC() {
@@ -70,14 +109,30 @@ final class LoginViewController: UIViewController {
     private func pushToWelcomeVC() {
         let welcomeViewController = WelcomeViewController()
         welcomeViewController.id = idTextField.text
+        welcomeViewController.date = datePickerTextField.text
         self.navigationController?.pushViewController(welcomeViewController, animated: true)
     }
     
     @objc
     private func loginButtonDidTap() {
-        presentToWelcomeVC()
+        guard let id = idTextField.text, !id.isEmpty else {
+            showAlert(message: "아이디를 입력해주세요.")
+            return
+        }
+        
+        guard let password = passwordTextField.text, !password.isEmpty else {
+            showAlert(message: "비밀번호를 입력해주세요.")
+            return
+        }
+        
+//        presentToWelcomeVC()
         pushToWelcomeVC()
     }
-    
+
+    private func showAlert(message: String) {
+        let alert = UIAlertController(title: "알림", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default))
+        present(alert, animated: true)
+    }
 }
 
--- 34th-Sopt-Seminar/34th-Sopt-Seminar/Source/VC/WelcomeViewController.swift
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/Source/VC/WelcomeViewController.swift
@@ -5,10 +5,26 @@
 //  Created by Seonwoo Kim on 3/30/24.
 //
 
-import Foundation
 import UIKit
 
 final class WelcomeViewController: UIViewController {
+    var id: String?
+    var date: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        setLayout()
+        bindData()
+    }
+    
+    private func setLayout() {
+        [logoImageView, welcomeLabel, goHomeButton, backToLoginButton, dateLabel].forEach {
+//            $0.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview($0)
+        }
+    }
     
     private let logoImageView: UIImageView = {
         let imageView = UIImageView(frame: CGRect(x: 112, y: 87, width: 150, height: 150))
@@ -18,30 +34,38 @@ final class WelcomeViewController: UIViewController {
     
     private let welcomeLabel: UILabel = {
         let label = UILabel(frame: CGRect(x: 140, y: 295, width: 95, height: 60))
-//        label.text = "???님 \n반가워요!"
-//        label.font = UIFont(name: "Pretendard-ExtraBold", size: 25)
+        label.font = UIFont(name: "Pretendard-ExtraBold", size: 25)
         label.textAlignment = .center
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 93, y: 335, width: 190, height: 100))
+        label.font = UIFont(name: "Pretendard-Medium", size: 18)
+        label.textAlignment = .center
+        label.text = "hi"
         label.numberOfLines = 2
         return label
     }()
     
     private lazy var goHomeButton: UIButton = {
         let button = UIButton(frame: CGRect(x: 20, y: 426, width: 335, height: 58))
-        button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+        button.backgroundColor = .primaryOrange
         button.setTitle("메인으로", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.addTarget(self, action: #selector(backToLoginButtonDidTap), for: .touchUpInside)
-//        button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
+        button.titleLabel?.font = UIFont(name: "Pretendard-Normal", size: 18)
         button.layer.cornerRadius = 6
         return button
     }()
     
     private var backToLoginButton: UIButton = {
         let button = UIButton(frame: CGRect(x: 20, y: 498, width: 335, height: 58))
-        button.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        button.backgroundColor = .gray200
         button.setTitle("로그인하기", for: .normal)
-        button.setTitleColor(UIColor(red: 172/255, green: 176/255, blue: 185/255, alpha: 1), for: .normal)
-        button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
+        button.setTitleColor(.gray500, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Pretendard-Normal", size: 18)
         button.layer.cornerRadius = 6
         return button
     }()
@@ -55,29 +79,18 @@ final class WelcomeViewController: UIViewController {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.view.backgroundColor = .white
-        setLayout()
-        bindID()
-    }
-    
-    var id: String?
-    
-    private func bindID() {
-        print(id)
+    private func bindData() {
         if let idText = id {
             self.welcomeLabel.text = "\(idText)님 \n반가워요!"
         } else {
-            print("error")
+            print("Error: ID가 존재하지 않습니다.")
+        }
+        
+        if let dateText = date {
+            self.dateLabel.text = dateText
+        } else {
+            print("Error: 날짜가 존재하지 않습니다.")
         }
     }
-    
-    private func setLayout() {
-        [logoImageView, welcomeLabel, goHomeButton, backToLoginButton].forEach {
-//            $0.translatesAutoresizingMaskIntoConstraints = false
-            self.view.addSubview($0)
-        }
-    }
+
 }
