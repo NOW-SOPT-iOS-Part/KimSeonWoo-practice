6a73fead96cac5c2e74f343b0e8e5ec2d7b04833
kim-seonwoo <sw204011@gmail.com>
2024-04-18 10:45:20 +0900

    [Feat/#3] Scrollview safeared 제한 풀기

--- 34th-Sopt-Seminar/34th-Sopt-Seminar/Source/VC/ScrollViewController.swift
+++ 34th-Sopt-Seminar/34th-Sopt-Seminar/Source/VC/ScrollViewController.swift
@@ -17,6 +17,8 @@ final class ScrollViewController: UIViewController {
     private var blueView = UIView()
     private var yellowView = UIView()
     private var purpleView = UIView()
+    private var tealView = UIView()
+    private var grayView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +26,14 @@ final class ScrollViewController: UIViewController {
         
         setLayout()
         setStyle()
-        
-        self.navigationController?.isNavigationBarHidden = true
     }
     
     private func setStyle() {
+        self.navigationController?.isNavigationBarHidden = true
+        
         scrollView.do {
-            $0.backgroundColor = .gray400
+            $0.backgroundColor = .green
+            $0.contentInsetAdjustmentBehavior = .never
         }
         
         greenView.do {
@@ -48,52 +51,73 @@ final class ScrollViewController: UIViewController {
         purpleView.do {
             $0.backgroundColor = .purple
         }
+        
+        tealView.do {
+            $0.backgroundColor = .systemTeal
+        }
+        
+        grayView.do {
+            $0.backgroundColor = .gray
+        }
     }
     
     private func setLayout() {
         let screenSize = UIScreen.main.bounds.size
         let centerHalf = screenSize.width / 2
+        let contentSize = screenSize.height / 1.5
         
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
-        
-        [greenView, blueView, yellowView, purpleView].forEach {
+        [greenView, blueView, yellowView, purpleView, grayView, tealView].forEach {
             contentView.addSubview($0)
         }
         
         scrollView.snp.makeConstraints {
-            $0.top.trailing.leading.bottom.equalToSuperview()
+            $0.edges.equalToSuperview()
         }
         
-        contentView.snp.makeConstraints { make in
-            make.edges.equalTo(scrollView)
-            make.width.equalTo(scrollView)
-            make.height.greaterThanOrEqualToSuperview().priority(.low) //
+        contentView.snp.makeConstraints {
+            $0.edges.width.equalToSuperview()
+            $0.height.equalTo(contentSize * 3)
         }
         
         greenView.snp.makeConstraints { make in
             make.top.leading.equalToSuperview()
-            make.height.equalTo(337)
+            make.height.equalTo(contentSize)
             make.width.equalTo(centerHalf)
         }
         
         blueView.snp.makeConstraints { make in
             make.top.trailing.equalTo(contentView)
-            make.height.equalTo(337)
+            make.height.equalTo(contentSize)
             make.width.equalTo(centerHalf)
         }
         
         yellowView.snp.makeConstraints { make in
             make.leading.equalTo(greenView)
             make.top.equalTo(greenView.snp.bottom)
-            make.height.equalTo(337)
+            make.height.equalTo(contentSize)
             make.width.equalTo(centerHalf)
         }
         
         purpleView.snp.makeConstraints { make in
             make.trailing.equalTo(blueView)
             make.top.equalTo(blueView.snp.bottom)
-            make.height.equalTo(337)
+            make.height.equalTo(contentSize)
+            make.width.equalTo(centerHalf)
+        }
+        
+        tealView.snp.makeConstraints { make in
+            make.trailing.equalTo(yellowView)
+            make.top.equalTo(yellowView.snp.bottom)
+            make.height.equalTo(contentSize)
+            make.width.equalTo(centerHalf)
+        }
+        
+        grayView.snp.makeConstraints { make in
+            make.trailing.equalTo(purpleView)
+            make.top.equalTo(purpleView.snp.bottom)
+            make.height.equalTo(contentSize)
             make.width.equalTo(centerHalf)
         }
     }
